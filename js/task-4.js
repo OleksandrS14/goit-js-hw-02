326a6cad8306c057f8c6c8659d613944e2aeb483
Oleksandr1451 <alekszzz154@gmail.com>
2024-08-04 20:43:36 +0300

    update js

--- js/task-4.js
+++ js/task-4.js
@@ -1,14 +1,30 @@
-function checkForSpam(message) {
-    const normalizedMessage = message.tolowerCase();
-    if (normalizedMessage.inclubes('span') || normalizedMessage.inclubes('sale')) {
-        return true;
+function getShippingCost(country) {
+    let price;
+
+    switch (country) {
+        case 'China':
+            price = 100;
+            break;
+        case 'Chile':
+            price = 250;
+            break;
+        case 'Australia':
+            price = 170;
+            break;
+        case 'Jamaica':
+            price = 120;
+            break;
+        default:
+        return "Sorry, there is no delivery to your country";
     }
-    return false;
+    return `Shipping to ${country} will cost ${price} credits`;
 }
-console.log(checkForSpam("Latest technology news")); // false
-console.log(checkForSpam("JavaScript weekly newsletter")); // false
-console.log(checkForSpam("Get best sale offers now!")); // true
-console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-console.log(checkForSpam("Trust me, this is not a spam message")); // true
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
+console.log(getShippingCost("Germany")); // "Sorry, there is no delivery to your country"
+console.log(getShippingCost("China")); // "Shipping to China will cost 100 credits"
+console.log(getShippingCost("Chile")); // "Shipping to Chile will cost 250 credits"
+console.log(getShippingCost("Jamaica")); // "Shipping to Jamaica will cost 120 credits"
+console.log(getShippingCost("Sweden")); // "Sorry, there is no delivery to your country"
+
+
+
